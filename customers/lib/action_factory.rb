57c6ad324b5e9517d5e9ded356a9bfdcb4849d99
Kyrre Havik <kyrremann@gmail.com>
2021-02-04 07:48:58 +0100

    feat: using one outbound queue

--- customers/lib/action_factory.rb
+++ customers/lib/action_factory.rb
@@ -8,13 +8,17 @@ class ActionFactory
     @connection = Bunny.new
     @connection.start
     @channel = @connection.create_channel
+    @queue = @channel.queue("outbound_actions")
   end
 
   def publish(action)
     return unless action
     Log.info(action)
 
-    queue = @channel.queue(action.bank_id)
-    @channel.default_exchange.publish(action.to_json, routing_key: queue.name)
+    payload = {
+      bank: action.band_id
+      action: = action
+    }
+    @channel.default_exchange.publish(payload.to_json, routing_key: @queue.name)
   end
 end
